88a899dd9eb4abf85361525880d66791b392cf46
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-01-09 14:12:18 +0700

    Fix type in config.py

--- bin/config.py
+++ bin/config.py
@@ -5,8 +5,8 @@ KAFKA_VERSION = "3.6.1"
 
 DELAY = 0.01
 NUM_PARTITIONS = 3
-KAFKA_BROKER: str = "localhost:9092"
-STORE_TOPIC: str = "store_tutorial_15"
+KAFKA_BROKER = "localhost:9092"
+STORE_TOPIC = "store_tutorial_15"
 STORE_CONSUMER_GROUP = "store_tutorial_15"
 PREDICTION_TOPIC = "prediction_tutorial_15"
 PREDICTION_CONSUMER_GROUP = "prediction_tutorial_15"
