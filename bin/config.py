e468b282fca896d2343d0ee1cb0a881b4375ac57
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-02-01 13:13:28 +0700

    Update: new config

--- bin/config.py
+++ bin/config.py
@@ -1,3 +1,21 @@
+import os
+from time import sleep, time
+from json import dumps as json_dumps
+from datetime import datetime, timedelta
+from typing import Optional, Callable, Any
+
+import pandas as pd
+import numpy as np
+
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.streaming.query import StreamingQuery
+
+import findspark
+
+findspark.init()
+
 SCALE_VERSION = "2.13"
 SPARK_VERSION = "3.5.0"
 KAFKA_VERSION = "3.6.1"
@@ -7,15 +25,22 @@ SPARK_PACKAGES = [
     f"org.apache.kafka:kafka-clients:{KAFKA_VERSION}",
 ]
 
-DELAY = 10  # seconds
-NUM_PARTITIONS = 3
-KAFKA_BROKER = "localhost:9092"
-STORE_TOPIC = "store_tutorial_15"
-STORE_CONSUMER_GROUP = "store_tutorial_15"
-PREDICTION_TOPIC = "prediction_tutorial_15"
-PREDICTION_CONSUMER_GROUP = "prediction_tutorial_15"
+pd.options.mode.copy_on_write = True
+
+DELAY = 20  # seconds
+EXPIRE_TIME = 3  # hours
+DATASETS_PATH = "datasets/"
 BATCH_FOLDER = "batch_folder/"
-JSON_SCHEMA_LIST = [
+KAFKA_BROKER = "localhost:9092"
+STORE_TOPIC = "STORE_TOPIC_"
+# EVENT_TOPIC = "EVENT_TOPIC_"
+EVENT_TOPIC = "TestStore"
+# SEGMENT_TOPIC = "SEGMENT_TOPIC_"
+SEGMENT_TOPIC = "TestSegment"
+# PREDICTION_TOPIC = "PREDICTION_TOPIC_"
+PREDICTION_TOPIC = "TestPrediction"
+
+STORE_SCHEMA_LIST = [
     "RequestID INT",
     "Boro STRING",
     "Yr INT",
@@ -30,4 +55,30 @@ JSON_SCHEMA_LIST = [
     "fromSt STRING",
     "toSt STRING",
     "Direction STRING",
+]
+
+EVENT_SCHEMA_LIST = [
+    "SegmentID INT",
+    "Direction STRING",
+    "Vol INT",
+    "Timestamp TIMESTAMP",
+]
+
+SEGMENT_SCHEMA_LIST = [
+    "SegmentID INT",
+    "Boro STRING",
+    "street STRING",
+    "fromSt STRING",
+    "toSt STRING",
+    "Lat DOUBLE",
+    "Long DOUBLE",
+]
+
+PREDICTION_SCHEMA_LIST = [
+    "SegmentID INT",
+    "Direction STRING",
+    "ds TIMESTAMP",
+    "yhat DOUBLE",
+    "min_history_ds TIMESTAMP",
+    "max_history_ds TIMESTAMP",
 ]
